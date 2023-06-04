fbfb5a835ad9740e5796b0fb73871835aa64d4f5
rifuhub <121163328+rifuhub@users.noreply.github.com>
2023-06-04 15:18:42 +0600

    Add files via upload

---
+++ BankAccount/src/default_package/BankAccountException.java
@@ -0,0 +1,13 @@
+package default_package;
+
+class BankAccountException extends Exception
+{
+	public BankAccountException()
+	{
+		super();
+	}
+	public BankAccountException(String message)
+	{
+		super(message);
+	}
+}
